9f3ee6ae48fa15ec0edee204c67517f65dbdcd20
yage <yage@yage.io>
2024-06-13 12:00:57 -0600

    cached output library

---
+++ zsh/lib/scwrypts/cache.module.zsh
@@ -0,0 +1,139 @@
+SCWRYPTS__CACHED_OUTPUT() {
+	eval "$(USAGE__reset)"
+	local USAGE__description="
+		Caches any successful shell operation for the current scwrypts runtime;
+		useful for operations which require heavy processing, but frequent access.
+
+		Keep in mind the following:
+			- Operations are hashed exclusively by the --cache-file argument; use of the
+			  same cache file for multiple different commands will result in erratic behavior,
+			  so make sure the filename is unique across the full scwrypts runtime
+
+			- Operations which have a failed shell exit code are not cached; operations
+			  MUST succeed the first time in order to be cached
+
+			- The full stdout of the operation is cached to RAM
+	"
+
+	local \
+		USE_CACHE=true CACHE_ARGS_COUNT=0 CACHE_FILE \
+		ARGS=() ARGS_FORCE=allowed \
+		;
+
+	eval "$ZSHPARSEARGS"
+
+	local CACHE_FILE_FULLPATH="$SCWRYPTS_TEMP_PATH/$CACHE_FILE"
+
+	case $USE_CACHE in
+		bypass ) ${ARGS[@]} ;;
+		true | reset )
+			local EXIT_CODE=0
+
+			[[ $USE_CACHE =~ reset ]] && rm -- "$CACHE_FILE_FULLPATH" &>/dev/null
+
+			[ -f "$CACHE_FILE_FULLPATH" ] || {
+				${ARGS[@]} > "$CACHE_FILE_FULLPATH"
+				EXIT_CODE=$?
+			}
+
+			case $EXIT_CODE in
+				0 ) cat "$CACHE_FILE_FULLPATH"
+					;;
+				* ) cat "$CACHE_FILE_FULLPATH" 2>/dev/null
+					ERROR "error running '${ARGS[@]}'"
+					rm -- "$CACHE_FILE_FULLPATH" &>/dev/null
+					;;
+			esac
+
+			return $EXIT_CODE
+			;;
+	esac
+}
+
+#####################################################################
+
+SCWRYPTS__CACHED_OUTPUT__ARGS() {
+	# local CACHE_ARGS=()
+	local PARSED=0
+	case $1 in
+		--use-cache )
+			PARSED=2
+			case $2 in
+				true | reset | bypass )
+					CACHE_ARGS+=(--use-cache $2)
+					;;
+
+				* ) ERROR "invalid --use-cache value '$2'" ;;
+			esac
+	esac
+	return $PARSED
+}
+
+SCWRYPTS__CACHED_OUTPUT__ARGS__usage() {
+	USAGE__options+="\n
+		--use-cache   (default true) one of {'true', 'reset', 'bypass'}
+	"
+
+	USAGE__description+="\n
+		(this function uses functions cached per scwrypts runtime)
+	"
+}
+
+#####################################################################
+
+SCWRYPTS__CACHED_OUTPUT__parse() {
+	# local USE_CACHE CACHE_ARGS_COUNT=0 CACHE_FILE
+	local PARSED=0
+	case $1 in
+		--use-cache )
+			PARSED=2
+			case $2 in
+				true | reset | bypass )
+					((CACHE_ARGS_COUNT+=1))
+					USE_CACHE=$2
+					;;
+				* ) ERROR "cannot set USE_CACHE to '$2'" ;;
+			esac
+			;;
+
+		--cache-file )
+			PARSED=2
+			CACHE_FILE=$2
+			;;
+	esac
+	return $PARSED
+}
+
+SCWRYPTS__CACHED_OUTPUT__parse__usage() {
+	USAGE__options+="\n
+		--cache-file   (required) runtime-unique filename for cached data
+		               for cache to be automatically cleared, make sure this is a simple filename (no directories)
+
+		--reset-cache    clear the cache before performing the operation
+		--bypass-cache   skip the cache for a clean operation, but don't clear existing or set new cache data
+	"
+
+	USAGE__args+="\n
+		remaining arguments are executed as a shell command
+	"
+}
+
+SCWRYPTS__CACHED_OUTPUT__parse__validate() {
+	case $USE_CACHE in
+		true | reset | bypass ) ;;
+		* ) ERROR "invalid value '$USE_CACHE' for USE_CACHE (are you missing 'local USE_CACHE=true'?)" ;;
+	esac
+
+	[[ "$CACHE_FILE" ]] && {
+		mkdir -p -- "$SCWRYPTS_TEMP_PATH/$(dirname -- "$CACHE_FILE")" \
+			|| ERROR "unable to create base directory '$SCWRYPTS_TEMP_PATH/$(dirname -- "$CACHE_FILE")'"
+
+		true
+	} || ERROR "missing cache file"
+
+	[[ ${#ARGS[@]} -gt 0 ]] \
+		|| ERROR "no command provided"
+
+	[[ $CACHE_ARGS_COUNT -le 1 ]] \
+		|| ERROR "too many '--use-cache' flags used"
+}
--- zsh/lib/scwrypts/environment/user.module.zsh
+++ zsh/lib/scwrypts/environment/user.module.zsh
@@ -6,132 +6,115 @@ use scwrypts/environment/template
 
 #####################################################################
 
-__SCWRYPTS_ENVIRONMENT_CACHE__USER="$SCWRYPTS_TEMP_PATH/environment.user.yaml"
-__SCWRYPTS_ENVIRONMENT_CACHE__SHELL="$SCWRYPTS_TEMP_PATH/environment.shell.yaml"
-rm -- "$__SCWRYPTS_ENVIRONMENT_CACHE__USER" "$__SCWRYPTS_ENVIRONMENT_CACHE__SHELL" &>/dev/null
-
 SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT() {
-	local RESET_CACHED_TEMPLATE=false
+	eval "$(USAGE__reset)"
+	local USAGE__description="
+		Generates a metadata-enriched environment YAML for the target environment.
+	" \
+		CACHE_ARGS=() \
+		ENVIRONMENT_NAME \
+		PARSERS=(
+			SCWRYPTS__CACHED_OUTPUT__ARGS
+			SCWRYPTS_ENVIRONMENT__PARSE_ENV_NAME
+			)
 
-	local ENVIRONMENT_NAME=$SCWRYPTS_ENV
-	local MODE=cached-default
+	eval "$ZSHPARSEARGS"
 
-	local _S ERRORS=0
-	while [[ $# -gt 0 ]]
-	do
-		_S=1
-		case $1 in
-			--reset-cache ) RESET_CACHED_TEMPLATE=true ;;
-			--environment-name )  # does not use cache
-				[ $2 ] && ((_S+=1)) || ERROR 'missing environment name' || break
-				MODE=specific-environment
-				ENVIRONMENT_NAME="$2"
-				;;
-		esac
-		shift $_S
-	done
+	##########################################
 
-	CHECK_ERRORS --no-fail || return $?
-
-	case $MODE in
-		cached-default )
-			[[ $RESET_CACHED_TEMPLATE =~ true ]] \
-				&& rm "$__SCWRYPTS_ENVIRONMENT_CACHE__USER" &>/dev/null
-
-			[ -f "$__SCWRYPTS_ENVIRONMENT_CACHE__USER" ] \
-				|| _SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT > "$__SCWRYPTS_ENVIRONMENT_CACHE__USER"
-
-			cat "$__SCWRYPTS_ENVIRONMENT_CACHE__USER"
-			;;
-
-		specific-environment )
-			[[ $RESET_CACHED_TEMPLATE =~ true ]] \
-				&& WARNING 'using --environment-name will never use cached template; no need for --reset-cache'
-
-			_SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT "$ENVIRONMENT_NAME"
-			;;
-	esac
-
+	SCWRYPTS__CACHED_OUTPUT ${CACHE_ARGS[@]} \
+		--cache-file environment.user.yaml \
+		-- \
+		_SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT "$ENVIRONMENT_NAME" \
+		;
 }
+
 
 SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT_SHELL_VALUES() {
-	# used primarily by __CHECK_ENV_VAR in scwrypts environments
-	#
-	# returns the shell-compatible user environment by
-	#	- moving inherited .PARENTVALUE to value (if value is empty)
-	#	- moving inherited .PARENTSELECTION to selection (if selection is empty)
-	#	- converting list 'values' to shell-arrays
-	local RESET_CACHED_TEMPLATE=false
+	eval "$(USAGE__reset)"
+	local USAGE__description="
+		used primarily by __CHECK_ENV_VAR in scwrypts environments
 
-	local ENVIRONMENT_NAME=$SCWRYPTS_ENV
-	local MODE=cached-default
+		returns yaml which contains shell-compatible lookup values
+		  - moving inherited .PARENTVALUE to value (if value is empty)
+		  - moving inherited .PARENTSELECTION to selection (if selection is empty)
+		  - converting list 'values' to shell-arrays
+	" \
+		CACHE_ARGS=() \
+		ENVIRONMENT_NAME \
+		PARSERS=(
+			SCWRYPTS__CACHED_OUTPUT__ARGS
+			SCWRYPTS_ENVIRONMENT__PARSE_ENV_NAME
+			)
 
-	local _S ERRORS=0
-	local PASSTHROUGH_ARGS=($@)
-	while [[ $# -gt 0 ]]
-	do
-		_S=1
-		case $1 in
-			--reset-cache ) RESET_CACHED_TEMPLATE=true ;;
-			--environment-name )  # does not use cache
-				[ $2 ] && ((_S+=1)) || ERROR 'missing environment name' || break
-				MODE=specific-environment
-				ENVIRONMENT_NAME="$2"
-				;;
-		esac
-		shift $_S
-	done
+	eval "$ZSHPARSEARGS"
 
-	CHECK_ERRORS --no-fail || return $?
+	##########################################
 
-	case $MODE in
-		cached-default )
-			[[ $RESET_CACHED_TEMPLATE =~ true ]] \
-				&& rm "$__SCWRYPTS_ENVIRONMENT_CACHE__SHELL" &>/dev/null
+	SCWRYPTS__CACHED_OUTPUT ${CACHE_ARGS[@]} \
+		--cache-file environment.shell.yaml \
+		-- \
+		_SCWRYPTS_ENVIRONMENT__CONVERT_SHELL_VALUES \
+			--environment-name "$ENVIRONMENT_NAME" \
+		;
+}
 
-			[ -f "$__SCWRYPTS_ENVIRONMENT_CACHE__SHELL" ] \
-				|| _SCWRYPTS_ENVIRONMENT__CONVERT_SHELL_VALUES ${PASSTHROUGH_ARGS[@]} > "$__SCWRYPTS_ENVIRONMENT_CACHE__SHELL"
+SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT_JSON() {
+	eval "$(USAGE__reset)"
+	local USAGE__description="
+		returns a JSON object containing live environment configurations
+		for the target environment
 
-			cat "$__SCWRYPTS_ENVIRONMENT_CACHE__SHELL"
-			;;
+		contains both .path.style.lookup path and ENVIRONMENT_VARIABLE lookup keys
+	" \
+		CACHE_ARGS=() \
+		ENVIRONMENT_NAME \
+		PARSERS=(
+			SCWRYPTS__CACHED_OUTPUT__ARGS
+			SCWRYPTS_ENVIRONMENT__PARSE_ENV_NAME
+			)
 
-		specific-environment )
-			[[ $RESET_CACHED_TEMPLATE =~ true ]] \
-				&& WARNING 'using --environment-name will never use cached template; no need for --reset-cache'
+	eval "$ZSHPARSEARGS"
 
-			_SCWRYPTS_ENVIRONMENT__CONVERT_SHELL_VALUES ${PASSTHROUGH_ARGS[@]}
+	##########################################
+
+	SCWRYPTS__CACHED_OUTPUT ${CACHE_ARGS[@]} \
+		--cache-file environment.user.json \
+		-- \
+		_SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT_JSON \
+			--environment-name "$ENVIRONMENT_NAME" \
+		;
+}
+
+#####################################################################
+
+SCWRYPTS_ENVIRONMENT__PARSE_ENV_NAME() {
+	# local ENVIRONMENT_NAME
+	local PARSED=0
+	case $1 in
+		--environment-name )
+			PARSED=2
+			ENVIRONMENT_NAME=$2
+
+			# bypass cache when specifying explicit environment name
+			[[ ${(t)CACHE_ARGS} =~ array ]] && CACHE_ARGS+=(--use-cache bypass)
 			;;
 	esac
+	return $PARSED
 }
 
-_SCWRYPTS_ENVIRONMENT__CONVERT_SHELL_VALUES() {
-	SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT $@ \
-		| YQ '..
-			|= select(
-				((has ("value") | not) or .value == null) and has (".PARENTVALUE")
-				).value = .".PARENTVALUE"
-			' \
-		| YQ '..
-			|= select(
-				((has ("selection") | not) or .selection == null or .selection | length == 0) and has (".PARENTVALUE")
-				).value = .".PARENTVALUE"
-			' \
-		| YQ '..
-			|= select(
-				has("value") and .value | type == "!!seq" and .value | length != 0
-				).value = "(" + (.value | join " ") + ")"
-			' \
-		| YQ '.
-			| del(.. | select(has(".PARENTVALUE")).".PARENTVALUE")
-			| del(.. | select(has(".ENVIRONMENT")).".ENVIRONMENT")
-			| del(.. | select(has(".GROUP")).".GROUP")
-			| del(.. | select(has(".DESCRIPTION")).".DESCRIPTION")
-			| del(.. | select(has("value") and .value == null).value)
-			| del(.. | select(has("selection") and (.selection == null or .selection | length == 0)).selection)
-			| del(.. | select(has("value") and has("selection")).selection)
-			' \
-			;
+SCWRYPTS_ENVIRONMENT__PARSE_ENV_NAME__usage() {
+	USAGE__options+="\n
+		--environment-name <string>   name of a scwrypts environment (default: $SCWRYPTS_ENV)
+		                              using this flag will bypass cache
+	"
 }
+
+SCWRYPTS_ENVIRONMENT__PARSE_ENV_NAME__validate() {
+	[ "$ENVIRONMENT_NAME" ] || ENVIRONMENT_NAME="$SCWRYPTS_ENV"
+}
+
+#####################################################################
 
 _SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT() {
 	local ENVIRONMENT_NAME LOADING_ORIGINAL_ENV=true
@@ -168,7 +151,7 @@ _SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT() {
 					# - metadata tags are READONLY; changes to any key which starts with a
 					#   '.' followed by all-caps (e.g. '.DESCRIPTION') will be ignored on
 					#   save
-					# 
+					#
 					# - value precedence is as follows (lower number is higher priority):
 					#    0. runtime environment variable '__override' value
 					#    1. the value from the 'value' key
@@ -194,7 +177,7 @@ _SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT() {
 
 	local GROUP GROUP_CONFIG_FILENAME
 	{
-		_SCWRYPTS_ENVIRONMENT__GET_FULL_TEMPLATE_WITH_VALUE_KEYS --environment-name "$ENVIRONMENT_NAME"
+		_SCWRYPTS_ENVIRONMENT__GET_FULL_TEMPLATE_WITH_VALUE_KEYS
 		[[ $LOADING_ORIGINAL_ENV =~ true ]] && {
 			for PARENT in $(_SCWRYPTS_ENVIRONMENT__GET_PARENT_ENV_NAMES "$ENVIRONMENT_NAME")
 			do
@@ -229,11 +212,60 @@ _SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT() {
 }
 
 _SCWRYPTS_ENVIRONMENT__GET_FULL_TEMPLATE_WITH_VALUE_KEYS() {
-	SCWRYPTS_ENVIRONMENT__GET_FULL_TEMPLATE $@ \
+	SCWRYPTS_ENVIRONMENT__GET_FULL_TEMPLATE \
 		| YQ '(.. | select(has(".ENVIRONMENT"))) += {
 				"selection": [],
 				"value": null
 			}
 			' \
 		| sed 's/ ""$//'
+}
+
+#####################################################################
+
+_SCWRYPTS_ENVIRONMENT__CONVERT_SHELL_VALUES() {
+	SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT $@ \
+		| YQ '..
+			|= select(
+				((has ("value") | not) or .value == null) and has (".PARENTVALUE")
+				).value = .".PARENTVALUE"
+			' \
+		| YQ '..
+			|= select(
+				((has ("selection") | not) or .selection == null or .selection | length == 0) and has (".PARENTVALUE")
+				).value = .".PARENTVALUE"
+			' \
+		| YQ '..
+			|= select(
+				has("value") and .value | type == "!!seq" and .value | length != 0
+				).value = "(" + (.value | join " ") + ")"
+			' \
+		| YQ '.
+			| del(.. | select(has(".PARENTVALUE")).".PARENTVALUE")
+			| del(.. | select(has(".ENVIRONMENT")).".ENVIRONMENT")
+			| del(.. | select(has(".GROUP")).".GROUP")
+			| del(.. | select(has(".DESCRIPTION")).".DESCRIPTION")
+			| del(.. | select(has("value") and .value == null).value)
+			| del(.. | select(has("selection") and (.selection == null or .selection | length == 0)).selection)
+			| del(.. | select(has("value") and has("selection")).selection)
+			' \
+			;
+}
+
+#####################################################################
+
+_SCWRYPTS_ENVIRONMENT__GET_USER_ENVIRONMENT_JSON() {
+	local SHELL_VALUES="$(_SCWRYPTS_ENVIRONMENT__CONVERT_SHELL_VALUES $@)"
+	local LOOKUP_MAP="$(SCWRYPTS_ENVIRONMENT__GET_ENVVAR_LOOKUP_MAP)"
+	{
+		echo "$SHELL_VALUES"
+		local ENVIRONMENT_VARIABLE
+		for ENVIRONMENT_VARIABLE in $(\
+			SCWRYPTS_ENVIRONMENT__GET_FULL_TEMPLATE \
+				| YQ '.. | select(has(".ENVIRONMENT")) | .".ENVIRONMENT"' \
+			)
+		do
+			echo "$ENVIRONMENT_VARIABLE: $(echo "$SHELL_VALUES" | YQ -r "$(echo "$LOOKUP_MAP" | YQ -r ".$ENVIRONMENT_VARIABLE")")"
+		done
+	} | YQ -oj
 }
