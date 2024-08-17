ebdd1be3ec4749cf7cff9c952f82ad9cc49e39e9
yage <yage@yage.io>
2024-08-17 08:56:02 -0600

    update plugins/* to use new scwrypts.zsh format

--- plugins/ci/ci.scwrypts.zsh
+++ plugins/ci/ci.scwrypts.zsh
@@ -1,5 +1,2 @@
-SCWRYPTS_GROUPS+=(ci)
-
-export SCWRYPTS_TYPE__ci=zsh
-export SCWRYPTS_ROOT__ci="$SCWRYPTS_ROOT__scwrypts/plugins/ci"
-export SCWRYPTS_COLOR__ci='\033[0m'
+export ${scwryptsgroup}__type=zsh
+export ${scwryptsgroup}__color='\033[0m'
--- plugins/kubectl/kubectl.scwrypts.zsh
+++ plugins/kubectl/kubectl.scwrypts.zsh
@@ -1,11 +1,10 @@
-SCWRYPTS_GROUPS+=(kubectl)
+export ${scwryptsgroup}__type=zsh
+export ${scwryptsgroup}__color='\033[0;31m'
 
-export SCWRYPTS_TYPE__kubectl=zsh
-export SCWRYPTS_ROOT__kubectl="$SCWRYPTS_ROOT__scwrypts/plugins/kubectl"
-export SCWRYPTS_COLOR__kubectl='\033[0;31m'
+#####################################################################
 
 SCWRYPTS_STATIC_CONFIG__kubectl+=(
-	"$SCWRYPTS_ROOT__kubectl/.config/static/redis.zsh"
+	"${scwryptsgrouproot}/.config/static/redis.zsh"
 )
 
-source "$SCWRYPTS_ROOT__kubectl/driver/kubectl.driver.zsh"
+source "${scwryptsgrouproot}/driver/kubectl.driver.zsh"
